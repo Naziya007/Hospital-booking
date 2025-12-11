04beaa5dcc84fa5244796cd217e4e495db1d9e40
Naziya Parveen <166731080+Naziya007@users.noreply.github.com>
2025-12-12 03:42:40 +0530

    Update CORS to allow deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,25 @@ import apointmentRoute from "./routes/apointmentRoute.js"
 dotenv.config();
 const app = express();
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173" }));  
+// app.use(cors({ origin: "http://localhost:5173" }));  
+// Allowed origins
+const allowedOrigins = [
+  "http://localhost:5173",                 // local dev
+  "https://hospital-booking-4.onrender.com" // deployed frontend
+];
+
+// Middlewares
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow Postman or server-to-server requests
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // allow cookies if needed
+}));
 app.use(express.json());
 
 
