14d6fc9e07b5ac29d6af2d98ccebaa8acc367b4c
Naziya Parveen <166731080+Naziya007@users.noreply.github.com>
2025-12-12 03:22:30 +0530

    adding deploy backend

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000", // backend URL
+  baseURL: "https://hospital-booking-1.onrender.com", // backend URL
 });
 
 // Token Auto Attach
